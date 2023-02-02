ae069caf4cc3db9a3d7825091b790d6a3ef53cc4
Ariaaf7 <aria.abdil_ti20@nusaputra.ac.id>
2023-02-02 16:21:06 +0700

    first commit

---
+++ resources/views/layouts/uts-layout.blade.php
@@ -0,0 +1,360 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <header class="w-full h-[145vh] lg:h-[750px]  bg-[#5C50E3]">
+        <div class=" flex justify-between py-5">
+
+            <div class="flex ml-10">
+                <img src="img/Logo.png" alt="">
+                <h1 class="ml-3 text-white ">CodingLearned</h1>
+            </div>
+            <nav>
+                <ul class="flex text-white gap-8">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Study Program</a><img src="img/Vector (2).png" alt="" class="absolute ml-[110px] top-7"></li>
+                    <li><a href="">Our Teacher</a></li>
+                    <li><a href="">Organisation</a><img src="img/Vector (2).png" alt=""class="absolute ml-[95px] top-7"></li>
+                    <li><a href="">Info</a></li>
+                    <li><a href="">News</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
+            <button type="button" class="mr-10 border-solid border-2 border-white py-2 px-8 rounded-full">Registration</button>
+
+
+
+        </div>
+
+        <div class="lg:ml-10 ml-6">
+            <h4 class="mt-10 lg:mt-32 text-[#FFCD52]">100% good at programming  🚀</h4>
+            <h1 class=" text-white text-3xl lg:text-5xl font-bold mt-5">Welcome to the
+              <br>  Coding School Website</h1>
+                <p class="text-sm text-[#DDDDDD] pb-17 mt-5">Lörem ipsum dobel nid rearad i polilogi osent. Nyck fabel otere och autotunt. Sas nivis <br> dekasalingar mikrotik. Krongar visade i vasebelt. </p>
+               <div class="flex">
+                <button type="button" class="bg-[#FFCD52] py-2 px-5 lg:py-3 lg:px-10 rounded-full mr-10 mt-10">Registration </button>
+                <img src="img/fluent_play-circle-20-regular.png" alt="" class=" mr-3 w-8  h-8 mt-12">
+                <button type="button" class="mt-10 Absolute -left-10 text-white">Play Now</button>
+               </div>
+              
+              
+                <div class="mt-[125%] lg:mt-20">
+                    <p class="text-white">Student</p>
+                    <div class=" flex absolute mt-5">
+                        <img src="img/Frame 1597882761.png" alt="" class="">
+                        <img src="img/Frame 1597882762.png" alt="" class="-ml-2">
+                        <img src="img/Frame 1597882763.png" alt="" class="-ml-2">
+                        <img src="img/Frame 1597882764.png" alt="" class="-ml-2">
+                        <img src="img/Frame 1597882765.png" alt="" class="-ml-2">
+                     
+                    </div>
+                    <div class="flex mt-16">
+                        <img src="img/Star 1.png" alt="" class="py-1 absolute">
+                        <h1 class=" text-white ml-6 mr-3">4.9</h1>
+                        <h2 class="text-[#FFFFFF]">(704.6k review)</h2>
+                    </div>
+
+                    <img src="img/Group 1597882786.png" alt="" class="absolute w-[300px]  top-[75%] lg:w-[620px] lg:left-[700px] lg:top-[155px]">
+                    <img src="img/Rectangle 7766.png" alt="" class="absolute w-[300px] top-[91%] lg:w-[620px] lg:left-[700px] lg:top-[370px]">
+                    <img src="img/Frame 1597882766.png" alt="" class="absolute w-[80px] top-[107%] left-2 lg:w-[230px] lg:left-[600px] lg:top-[580px]">
+                    <img src="img/Frame 1597882767.png" alt=""class="absolute w-[90px] top-[73%] left-[80px] lg:w-[230px] lg:left-[750px] lg:top-[130px]">
+                    <img src="img/Frame 1597882768.png" alt=""class="absolute w-[100px] top-[92%] left-[190px] lg:w-[230px] lg:left-[1030px] lg:top-[400px]">
+
+
+                </div>
+               
+            </div>
+    </header>
+
+    <section>
+        <div class="w-full h-[120vh] bg-white">
+            <div class="flex gap-3 mt-5 ml-2 lg:gap-10 lg:mt-20 lg:ml-[400px]">
+                <img src="img/Vector.png" alt="" class="lg:w-[100px] lg:h-7 w-10 h-5">
+                <img src="img/Amazon logo.png" alt="" class="lg:w-[100px] lg:h-10 w-10 h-10">
+                <img src="img/google logo.png" alt="" class="lg:w-[100px] lg:h-10 w-10 h-10">
+                <img src="img/microsoft logo.png" alt="" class="lg:w-[100px] lg:h-7 w-14 h-10">
+                <img src="img/Frame 182.png" alt="" class="lg:h-10 lg:w-[140px] w-20 h-10">
+            </div>
+
+
+            <div class="text-center mt-8 lg:mt-20">
+                <img src="img/Group 1597882816.png" alt="" class="lg:ml-[536px] lg:w-[400px] w-[300px] ml-3">
+            </div>
+            <div class="lg:flex ml-[150px] gap-10">
+
+                <div class=" mt-10">
+                    <img src="img/Frame 1597882777.png" alt="" class="lg:static relative  -ml-[70px] lg:ml-0">
+                    <h1 class="text-1xl lg:text-3xl font-bold mt-4 lg:mb-5 mb-1 lg:static relative -ml-[80px] lg:ml-0">Creating a Web Login and <br> Todolist Using Laravel</h1>
+                    <p class="text-sm lg:static relative -ml-[80px] lg:ml-0">Lörem ipsum mögunde seråpp pos. Faplasade jer och titeck </p>
+                    <div class="flex lg:mt-5 mt-2">
+                        <img src="img/Group 1597882817.png " alt="" class="mr-10 lg:static relative -ml-[80px] lg:ml-0">
+                        <p>(4,2)</p>
+                    </div>
+                    <h1 class="lg:ml-[320px] font-bold lg:text-2xl text-[#5C50E3] ml-[110px]">$ 35</h1>
+                    <div class="flex">
+                        <img src="img/ant-design_play-circle-outlined.png" alt="" class="lg:static relative -left-20 ">
+                        <p class="ml-3 lg:static relative -left-20">8 Video</p>
+                    </div>
+                </div>
+                <div class=" mt-10">
+                    <img src="img/Frame 1597882778.png" alt="" class="lg:static relative  -ml-[70px] lg:ml-0">
+                    <h1 class="text-1xl lg:text-3xl font-bold mt-4 lg:mb-5 mb-1 lg:static relative -ml-[80px] lg:ml-0">Web Login Management <br> with PHP and MySQL</h1>
+                    <p class="text-sm lg:static relative -ml-[80px] lg:ml-0">Lörem ipsum mögunde seråpp pos. Faplasade jer och titeck </p>
+                    <div class="flex mt-5">
+                        <img src="img/Group 1597882817.png" alt="" class="mr-10 lg:static relative -ml-[80px] lg:ml-0">
+                        <p>(4,2)</p>
+                    </div>
+                    <h1 class="ml-[320px] font-bold text-2xl text-[#5C50E3] ml-[110px]">$ 20</h1>
+                    <div class="flex">
+                        <img src="img/ant-design_play-circle-outlined.png" alt="">
+                        <p class="ml-3 lg:static relative -left-20">8 Video</p>
+                    </div>
+                </div>
+                <div class=" mt-10">
+                    <img src="img/Frame 1597882779.png" alt="">
+                    <h1 class="text-3xl font-bold mt-4 mb-5">Build a CMS Website With <br> Laravel and Nuxt Js</h1>
+                    <p class="text-sm ">Lörem ipsum mögunde seråpp pos. Faplasade jer och titeck </p>
+                    <div class="flex mt-5">
+                        <img src="img/Group 1597882817.png" alt="" class="mr-10">
+                        <p>(4,2)</p>
+                    </div>
+                    <h1 class="ml-[320px] font-bold text-2xl text-[#5C50E3]">$ 40 </h1>
+                    <div class="flex">
+                        <img src="img/ant-design_play-circle-outlined.png" alt="">
+                        <p class="ml-3">8 Video</p>
+                    </div>
+                   
+                </div>
+                
+            </div>
+           
+                <button type="button" class="relative ml-[680px] mt-10 rounded-full py-2 px-6 bg-[#5C50E3] text-white ">See more</button>
+         
+        </div>
+    </section>
+
+    <section class="w full h-[549px] bg-slate-200">
+        <div class="ml-20 py-28">
+         <h2 class="text-[#FFCD52]">About Us 🏫</h2> 
+         <img src="img/Vector 64.png" alt="" class="absolute w-[100px] mt-[55px] left-[320px]">
+         <h1 class="mt-5 text-3xl font-bold mb-5">Professional and Quality<br>Teacher</h1>
+         <p>Lörem ipsum mögunde seråpp pos. Faplasade jer och titeck såryledes.<br> Eurossade gesat. Usoligen. Nåhet exonde rytubel der. </p>
+            <button type="button" class="mt-10 rounded-full py-2 px-6 bg-[#5C50E3] text-white">Search Course</button>
+        </div>
+        <div class="absolute left-[800px] top-[1689px]">
+            <img src="img/Group 1597882833.png" alt="" class="left-[800px] top-[1689px]">
+            <img src="img/Vector (2).png" alt="">
+            <img src="img/Vector (2).png" alt="">
+
+        </div>
+        
+    </section>
+    <section class="w full h-[549px]">
+        <div class="flex">
+            <div class=" mr-10 ml-40 py-28">
+                <img src="img/Image.png" alt="">
+                <h1 class="mb-3 mt-5 font-bold">Fun Teacher</h1>
+                <p>Lörem ipsum mögunde seråpp pos.<br> Faplasade jer och </p>
+            </div>
+            <div class="py-28">
+                <img src="img/Image (2).png" alt="">
+                <h1 class="mb-3 mt-5 font-bold">500 Expert Student</h1>
+                <p>Lörem ipsum mögunde seråpp pos.<br> Faplasade jer och </p>
+            </div>
+          <div class="ml-20 py-20">
+            <h1 class="text-[#FFCD52]">Why Choose Us</h1>
+            <img src="img/Vector 64.png" alt="" class="absolute mt-[100px] w-[100px] left-[980px]">
+            <p class="mt-5 text-4xl font-bold">Benefits of online Tutoring <br> Services at Coding <br> Learned</p>
+            <p class="mt-5">Lörem ipsum mögunde seråpp pos. Faplasade jer och titeck såryledes. Eurossade gesat.<br> Usoligen. Nåhet exonde rytubel der. </p>
+         <div class="flex gap-8">
+            <button type="button" class="mt-10 rounded-full py-2 px-6 bg-[#5C50E3] text-white">Registration</button>
+            <button type="button" class="mt-7 underline underline-offset-1">Program Study</button>
+            <img src="img/Vector (3).png" alt="" class="mt-[50px] w-5 h-5">
+         </div>
+
+        </div>
+            
+
+        </div>
+        <div class="flex relative -top-[200px]">
+            <div class=" mr-10 ml-40 py-28">
+                <img src="img/Image (1).png" alt="">
+                <h1 class="mb-3 mt-5 font-bold">24/7 Tutor Availability</h1>
+                <p>Lörem ipsum mögunde seråpp pos.<br>Faplasade jer och  </p>
+            </div>
+            <div class="py-28">
+                <img src="img/Image (3).png" alt="">
+                <h1 class="mb-3 mt-5 font-bold">Easy Payment</h1>
+                <p>Lörem ipsum mögunde seråpp pos.<br> Faplasade jer och </p>
+                
+            </div>
+            
+        </div>
+       
+
+
+    </section>
+    <section class="w full h-[549px] bg-slate-200">
+        <div class="ml-20 py-28">
+            <img src="img/Vector 64.png" alt="" class="absolute mt-[80px] w-[120px] left-[205px]">
+            <h1 class="text-[#FFCD52]">Owner Said</h1>
+            <p class="mt-5 text-4xl font-bold">Owner Coding <br>Learned</p>
+            <p class="mt-5">“ Lörem ipsum mögunde seråpp pos. Faplasade jer och titeck såryledes.<br>Eurossade gesat. Usoligen. Nåhet exonde rytubel der. “</p>
+   
+           <div class="flex">
+               <div>
+                <h1 class="absolute font-bold ml-12 mt-8 pb-2">Prof. Nugraha</h1>
+                <h2 class="absolute text-[#FFFFFF] ml-12 mt-12">Owner Coding Learned</h2>
+                <img src="img/Ellipse 77.png" alt="" class="mt-8">
+            </div>
+            <div class="absolute left-[800px] top-[390%]">
+                <img src="img/yt.png" alt="">
+            </div>
+        </div>
+
+           
+        </div>
+    </section>
+    <section class="w full h-[400px] bg-white">
+        <div class="mt-[220px] ml-20">
+            <img src="img/Vector 68 (1).png" alt="" class="absolute top-[468%] ml-[44%]">
+            <img src="img/Group 1597882835.png" alt="" class="absolute ml-[600px] top-[465%]">
+            <img src="img/Vector 64.png" alt="" class="absolute mt-[70px] w-[100px] left-[180px]">
+            <h1 class="text-3xl font-bold">What Students Say <br> About Coding Learned</h1>
+            <p class="mt-5">Lörem ipsum mögunde seråpp pos. Faplasade jer och titeck såryledes.<br> Eurossade gesat. Usoligen. Nåhet exonde rytubel der. </p>
+            <div class="absolute left-[800px] top-[1689px]">
+                <img src="img/Group 1597882833.png" alt="" class="left-[800px] top-[1689px]">
+        </div>
+    </section>
+    <section class="w full h-[700px] bg-slate-100">
+        
+        <div class="py-[80px]">
+            <img src="img/Vector 64.png" alt="" class="absolute mt-[30px] w-[120px] left-[730px]">
+            <h1 class="text-center text-3xl font-semibold">Our Teacher</h1>
+            <p class="text-center py-5 text-sm">Lörem ipsum mögunde seråpp pos. Faplasade jer och titeck </p>
+            <div class="flex gap-10 justify-center items-center py-10">
+                <div class="w-[186px] h-[200px] ">
+                    <img src="img/Vector (5).png" alt="" class="absolute mt-[115px] -ml-3">
+                    <img src="img/pngwing 7.png" alt="" class="">
+                    <div class="text-center ">
+                        <h1 class="py-3 font-semibold">Jacob Jones</h1>
+                        <p class="text-xs text-[#5A5A5A]">Math Teacher</p>
+                    </div>
+                </div>
+                <div class="w-[186px] h-[200px] bg-black">
+                    <img src="img/pngwing 6.png" alt="" class="relative -top-2">
+                    <div class="text-center ">
+                        <h1 class="py-3 font-semibold">Jacob Jones</h1>
+                        <p class="text-xs text-[#5A5A5A]">Math Teacher</p>
+                    </div>
+                </div>
+                <div class="w-[186px] h-[200px] from-slate-100 to-[#ddd6fe] bg-gradient-to-b">
+                    <img src="img/pngwing 3.png" alt="" class="relative -top-1">
+                    <div class="text-center ">
+                        <h1 class="py-3 font-semibold">Jacob Jones</h1>
+                        <p class="text-xs text-[#5A5A5A]">Math Teacher</p>
+                    </div>
+                </div>
+                <div class="w-[186px] h-[200px] bg-black">
+                    <img src="img/pngwing 5.png" alt="" class="mt-3">
+                    <div class="text-center ">
+                        <h1 class="mt-5 font-semibold">Jacob Jones</h1>
+                        <p class="text-xs text-[#5A5A5A] mt-3">Math Teacher</p>
+                    </div>
+                </div>
+                <div class="w-[186px] h-[200px] bg-black">
+                    <img src="img/Vector (4).png" alt="" class="absolute mt-[100px] ml-[180px]">
+                    <img src="img/pngwing 4 (1).png" alt="" class="mt-8">
+                    <div class="text-center ">
+                        <h1 class="mt-5 font-semibold">Jacob Jones</h1>
+                        <p class="text-xs mt-3 text-[#5A5A5A]">Math Teacher</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="mt-24 text-center">
+                <button type="button" class="py-2 px-6 bg-[#5C50E3] rounded-full text-white">See more</button>
+            </div>
+        </div>
+    </section>
+
+    <section class="w full h-[500px] bg-[#030024]">
+        <div class="flex text-white">
+            <div class="py-20 ml-32">
+
+                <div class="flex gap-3">
+                    <img src="img/Logo (1).png" alt="">
+                    <h1 class="font-semibold">Coding Learned</h1>
+                </div>
+                <p class="mt-8">Lörem ipsum dobel nid rearad i polilogi osent. Nyck fabel<br> otere och autotunt. Sas nivis dekasalingar mikrotik. Krongar <br> visade i vasebelt.  </p>
+                <div class="flex gap-5 mt-10">
+                    <img src="img/facebook logo.png" alt="">
+                    <img src="img/Twitter logo.png" alt="">
+                    <img src="img/Vector (6).png" alt="">
+                    <img src="img/Vector (7).png" alt="">
+                </div>
+            </div>
+            <div class="mt-32 ml-14">
+                <h1 class="font-bold">Home</h1>
+                <div>
+                   <nav>
+                    <ul>
+                        <li class="mt-5"><a href="">Course</a></li>
+                        <li class="mt-5"><a href="">Teacher</a></li>
+                        <li class="mt-5"><a href="">Study</a></li>
+                        <li class="mt-5"><a href="">About </a></li>
+                        <li class="mt-5"><a href="">Contact</a></li>
+                    </ul>
+                   </nav>
+                </div>
+            </div>
+            <div class="mt-32 ml-20">
+                <h1 class="font-bold">Feature</h1>
+                <div>
+                   <nav>
+                    <ul>
+                        <li class="mt-5"><a href="">Payment</a></li>
+                        <li class="mt-5"><a href="">Virtual Account</a></li>
+                        <li class="mt-5"><a href="">Go-pay</a></li>
+                        
+                    </ul>
+                   </nav>
+                </div>
+            </div>
+            <div class="mt-32 ml-20 ">
+                <h1 class="font-bold">Company</h1>
+                <div>
+                   <nav>
+                    <ul>
+                        <li class="mt-5"><a href="">Carrees</a></li>
+                        <li class="mt-5"><a href=""> Contact</a></li>
+                        <li class="mt-5"><a href="">FAQ</a></li>
+                        <li class="mt-5"><a href="">Partners</a></li>
+                        <li class="mt-5"><a href="">Privacy & Policy</a></li>
+                    </ul>
+                   </nav>
+                </div>
+            </div>
+            <div class="mt-32 ml-14">
+                <h1>Subscribe to cheer us on</h1>
+                <form class="mt-5">
+                    <button type="" class="absolute ml-[240px] mt-1 py-3 px-3 bg-[#5C50E3] rounded-full"><img src="img/Vector (8).png" alt=""></button>
+                    <input type="text" id="fname" name="fname" class="py-3 px-14 rounded-full border-none text-black"><br>
+                   
+                  </form>
+
+                <p class="mt-[250px]">Copy right by mangcoding team 2022</p>
+                
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
